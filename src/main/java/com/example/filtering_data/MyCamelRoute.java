4905e7b788428b27ea38823bb9c304cca6ced5c2
Melbin Mathew <melmathew710@gmail.com>
2023-07-11 22:52:33 +0530

    first commit

---
+++ src/main/java/com/example/filtering_data/MyCamelRoute.java
@@ -0,0 +1,44 @@
+package com.example.filtering_data;
+
+//import org.apache.camel.Header;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MyCamelRoute extends RouteBuilder {
+
+    @Value("${pageNo}")
+    private Integer pageNo;
+
+    @Value("${ids}")
+    private String ids;
+
+    @Override
+    public void configure() throws Exception {
+        rest()
+                .get("/filter-data")
+                .produces("application/json")
+                .to("direct:start");
+
+        from("direct:start")
+                .setHeader("CamelHttpMethod", constant("GET"))
+                .toD("https://reqres.in/api/users?page=${property.pageNo}")
+                .unmarshal().json()
+                .filter().method(this, "filterIds")
+                .marshal().json(true)
+                .convertBodyTo(String.class)
+                .log("Filtered data: ${body}");
+    }
+
+    public boolean filterIds(String jsonPath) {
+        String[] idArray = ids.split(",");
+        for (String id : idArray) {
+            if (jsonPath.contains("\"id\":" + id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/com/example/filtering_data/FilteringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filtering_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilteringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
